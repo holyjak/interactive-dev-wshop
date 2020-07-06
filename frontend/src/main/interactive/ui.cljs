89284f574b18c7d9bcad2ca0359285c54defb07e
Jakub Holy <jakub.holy@telia.no>
2020-07-06 14:51:19 +0200

    Final server and frontend code

--- frontend/src/main/interactive/ui.cljs
+++ frontend/src/main/interactive/ui.cljs
@@ -1,33 +1,146 @@
 (ns interactive.ui
   (:require
    [clojure.edn :as edn]
-   [helix.core :refer [defnc $]]
+   [clojure.string :as str]
+   [helix.core :refer [<> defnc $]]
    [helix.hooks :as hooks]
    [helix.dom :as d]
+   ["react" :as react]
    ["react-dom" :as rdom]
-   ["semantic-ui-react" :as semantic]))
+   ["semantic-ui-react" :as semantic :refer [Header Message Modal]]))
 
 (defn init [])
 
-(defn fetch-people []
-  (-> (js/fetch "/api/people" #js {"method" "POST", "body" []})
-      (.then #(.text %))
-      (.then edn/read-string)))
+(defn handle-response 
+  ([uri resp] (handle-response "fetch" uri resp))
+  ([action uri resp]
+   (-> (if (.-ok resp)
+         (.then (.text resp) edn/read-string)
+         (.reject js/Promise (js/Error. (str "Request returned non-OK status " (.-status resp)))))
+       (.catch (fn [err]
+                 (js/console.error "Failed to " action uri err)
+                 ::error)))))
 
-(defnc person [{:keys [fname email]}]
-  (d/li (str fname " " email)))
+(defn fetch-people []
+  (let [uri "/api/people"]
+    (-> (js/fetch uri #js {"method" "POST", "body" (pr-str [:fname :email])})
+       (.then (partial handle-response uri)))))
+
+(defn fetch-person [email]
+  (let [uri (str "/api/person/" email)]
+    (-> (js/fetch uri #js {"method" "POST", "body" (pr-str [:fname :lname :email])})
+        (.then (partial handle-response uri)))))
+
+(defn update-person [email new-fname]
+  (let [uri (str "/api/person/" email)]
+    (-> (js/fetch uri #js {"method" "PUT", "body" (pr-str {:fname new-fname})})
+        (.then (partial handle-response "PUT" uri))
+        (.then #(when (= ::error %) (throw (js/Error. "Failed")))))))
+
+;; ===================================================================================================================
+
+(def my-context (react/createContext "default"))
+
+(defnc person-modal [{:keys [email]}]
+  (let [[state set-state] (hooks/use-state {:open? false, :person nil, :saving? false, :error? false})
+        loadfn (fn [] (set-state assoc :open? true)
+                 (-> (fetch-person email) (.then (fn [p] (set-state assoc :person p)))))
+        button (d/button {:className "ui button"
+                          :onClick loadfn}
+                         "Edit")]
+    ;(hooks/use-effect :once (loadfn)) ;; FIXME rm
+    ($ Modal {:trigger button
+              :open (:open? state)
+              :onClose (fn [] (set-state assoc :open? false))}
+       (let [{:keys [fname lname email] :as person} (:person state)
+             reload (:reload (hooks/use-context my-context))]
+         (<>
+          ($ Header {:content (str "Edit " fname " " lname)})
+          (cond
+            (= ::error person)
+            ($ Message {:error true} "Loading failed, see the Console for details")
+
+            (nil? person)
+            (d/p {:className "ui active loader"} "Loading...")
+
+            :else
+            (d/form
+             {:className (str/join " " ["ui form" (when (:error? state) "error")]) :style {:padding " 1em"}}
+             (d/div {:className "field"}
+                    (d/label "First name")
+                    (d/input {:type "text", :defaultValue fname, :on-change #(set-state assoc-in [:person :fname] (.. % -target -value))}))
+             (d/div {:className "disabled field"}
+                    (d/label "Last name")
+                    (d/input {:type "text", :value lname, :readOnly true}))
+             (d/div {:className "disabled field"}
+                    (d/label "Email")
+                    (d/input {:type "text", :value email, :readOnly true}))
+             (when true ;(:error? state)
+               (d/div {:className "ui error message"}
+                      (d/p "Saving the person failed - see the Console for details")))
+             (d/button {:className (str/join " " ["ui primary submit button" (when (:saving? state) "loading")]) 
+                        :type "submit"
+                        :on-click (fn [evt]
+                                    (.preventDefault evt)
+                                    (set-state assoc :saving? true)
+                                    (-> (update-person email fname)
+                                        (.then (fn [_]
+                                                 (set-state assoc :open? false)
+                                                 (reload)
+                                                 (set-state assoc :saving? false)))
+                                        (.catch (fn [_]
+                                                  (set-state assoc :error? true)
+                                                  (set-state assoc :saving? false)))))} 
+                       "Save"))))))))
+
+(defnc person [{:keys [fname email] :as person}]
+  (let [error-td #(d/td {:colSpan 3} ($ Message %))]
+   (d/tr 
+    (cond
+      (< (count person) 2) (error-td "An incomplete person this is!")
+      (> (count person) 2) (error-td "Too many attributes this person has!")
+      (not= (set (keys person)) #{:fname :email}) (error-td "A confusion of attributes befell us!")
+      :else
+      (<>
+       (d/td fname)
+       (d/td email)
+       (d/td ($ person-modal {:email email}))
+       #_(d/td (d/button {:className "ui button"
+                        :onClick #(println "edit" %)} 
+                       "Edit")))))))
+
+;(.-Content Modal)
 
 (defnc people [{:keys [people]}]
   (d/div 
-   (d/h3 "People")
-   (d/ul
-    (map #($ person {:key (:fname %) & %}) people))))
+   (d/h3 "My Clojure heroes")
+   (if (seq people)
+     (d/table {:className "ui celled striped table"}
+      (d/thead
+       (d/tr
+        (d/th "Name")
+        (d/th "E-mail")
+        (d/th {:className "one wide"} "")))
+      (d/tbody
+       (map #($ person {:key (or (:fname %) %) & %}) people)))
+     ($ Message {} "It is so lonely here, with no people around!"))))
 
-(defnc app [props]
-  (d/div
-   (d/h1 "Welcome!")
-   ($ people {& props})))
+(defnc app []
+  (let [[people-list set-people-list] (hooks/use-state [])
+        reload #(-> (fetch-people) (.then set-people-list))]
+    (hooks/use-effect :once (reload))
+    (helix.core/provider
+     {:context my-context
+      :value {:reload reload}}
+     (d/div 
+      (d/div {:className "ui fixed inverted menu"}
+             (d/div {:className "ui container"}
+                    (d/div {:className "header item"} "Interactive development workshop")))
+      (d/div {:className "ui main text container" :style {:margin-top "2.85714em"}}
+             (d/h1 {:className "ui header"} "Welcome to the interactive development workshop!")
+             (if (= ::error people-list)
+               ($ Message {:error true} "Fetching people failed - see the Console for details")
+               ($ people {:people people-list, :reload reload})))))))
 
 ;; start your app with your favorite React renderer
-(-> (fetch-people)
-    (.then #(rdom/render ($ app {:people %}) (js/document.getElementById "app"))))
+(rdom/render ($ app) (js/document.getElementById "app"))
--- src/interactive/server.clj
+++ src/interactive/server.clj
@@ -1,19 +1,25 @@
 (ns interactive.server
-  ""
+  "Backend for the Interactive Development Workshop"
   {:author "Jakub Holý"}
-  (:require 
+  (:require
+   [clojure.edn :as edn]
    [next.jdbc :as jdbc]
    [ring.adapter.jetty]
    [ring.middleware.resource]))
 
-;;-------------------------------------------------------- DATABASE
+;;-------------------------------------------------------- DATABASE (DO NOT TOUCH!)
 
 ;; A connection to an in-memory SQL database
-(def ds 
+(def ds
   (jdbc/get-datasource {:dbtype "h2" :dbname "temporary"}))
 
 (defn fetch-all-people []
   (jdbc/execute! ds ["SELECT * from people"]))
+
+(defn update-person-first-name [id first-name]
+  (assert id "id is required")
+  (assert first-name "first-name is required")
+  (jdbc/execute! ds ["UPDATE people SET fname=? WHERE id=?" first-name id]))
 
 (comment
   ;; TODO Move it off to a separate file, load upon start, add "reset" instructions
@@ -24,7 +30,7 @@ create table people (
   lname varchar (32),
   email varchar(255)
 )"])
-  
+
   (jdbc/execute! ds ["
 insert into people(fname,lname,email) values
   ('Rich', 'Hickey','rich@example.com'),
@@ -35,45 +41,93 @@ insert into people(fname,lname,email) values
 ;;-------------------------------------------------------- REQUEST HANDLING
 
 (defn handle-people [_req]
+  (println "_req" _req)
+  ;; TODO Fetch the people from the DB using `fetch-all-people` and `map` it to what the UI wants 
   {:status 200
-   :headers {"Content-Type" "application/edn"}
-   :body (pr-str [{:fname "Rich" :email "rich@example.com"}
-                  {:fname "Rich 2" :email "rich2@example.com"}])})
+   :headers {"Content-Type" "text/plain"}
+   :body (pr-str (->> (fetch-all-people)
+                      (map (fn [p] {:fname (:PEOPLE/FNAME p)
+                                    :email (:PEOPLE/EMAIL p)}))) #_[{:fname "Rich" :email "rich@example.com"}])})
+
+(defn handle-person [req]
+  (let [id (->> req :uri (re-find #"^/api/person/(.*)") second)]
+    ;; TODO Find the person in the DB using `fetch-all-people` and `filter` and `first` and transform it into the form the UI wants
+    {:status 200
+     :headers {"Content-Type" "text/plain"}
+     :body (pr-str (->> (fetch-all-people)
+                        (filter (fn [p] (= id (:PEOPLE/EMAIL p))))
+                        (map (fn [p] {:fname (:PEOPLE/FNAME p)
+                                      :lname (:PEOPLE/LNAME p)
+                                      :email (:PEOPLE/EMAIL p)}))
+                        (first)))}))
+
+(defn handle-person-update [req]
+  ;; TODO Find the person in the DB using `fetch-all-people` and `filter` and `first`
+  (let [email (->> req :uri (re-find #"^/api/person/(.*)") second)
+        person    (->> (fetch-all-people)
+                       (filter (fn [p] (= email (:PEOPLE/EMAIL p))))
+                       (first))
+        id    (:PEOPLE/ID person)]
+    (update-person-first-name id (-> req :body :fname))
+    {:status 200 :headers {"Content-Type" "text/plain"}}))
+
+;;-------------------------------------------------------- ROUTING, SERVER ETC. (DO NOT TOUCH)
 
 (defn api-handler [req]
   (cond
-    ;; (not= (:method req) :POST)
-    ;; {:status 400 :body "Only POST supported"}
-    
-    (re-find #"/api/people" (:uri req))
-    (handle-people req)))
+    (re-find #"^/api/people$" (:uri req))
+    (handle-people req)
 
-(defn handler [req]  
-  (def *req req)
+    (and
+     (re-find #"^/api/person/" (:uri req))
+     (= :post (:request-method req)))
+    (handle-person req)
+
+    (and
+     (re-find #"^/api/person/" (:uri req))
+     (= :put (:request-method req)))
+    (handle-person-update req)
+
+    :else
+    {:status 404 :body (str "Unsupported uri " (:uri req))}))
+
+(defn handler [req]
   (cond
     (re-find #"^/api/" (:uri req))
     (api-handler req)
-    
+
     (re-find #"^/?$" (:uri req))
     {:status 302 :headers {"location" "/index.html"}}
-    
+
     :else
     {:status 404 :body "So sad!!!"}))
 
+(defn try-parse-edn [body]
+  (try
+    (if body (-> body slurp edn/read-string))
+    (catch Exception _ignored
+      body)))
+
+(defn wrap-edn-request [handler]
+  (fn [req]
+    (-> (update req :body try-parse-edn)
+        (handler))))
+
 (def app
-  (-> (var handler)
+  (-> handler
       ;; Add wrap-content-type ? wrap-params? json?
+      (wrap-edn-request)
       (ring.middleware.resource/wrap-resource "public")))
 
 (defn -main "For `lein run`, etc." []
   (println "Starting example Ring server...")
-  (ring.adapter.jetty/run-jetty app {:port 8088 :join? false})
+  (ring.adapter.jetty/run-jetty (var app) {:port 8088 :join? false})
   (let [uri "http://localhost:8088/"]
     (println (str "Jetty running on: " uri))
     (try
       (.browse (java.awt.Desktop/getDesktop) (java.net.URI. uri))
       (catch java.awt.HeadlessException _))))
 
-(comment 
+(comment
   (-main)
   (app {:uri "/api/people" :method :post :body "[]"}))
